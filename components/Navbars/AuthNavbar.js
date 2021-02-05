3df6b1d055af73922165d18aae6ef660967f9d7c
Sekmet <sekmet@open4g.com>
2021-02-05 20:24:20 -0300

    add logo images

--- components/Navbars/AuthNavbar.js
+++ components/Navbars/AuthNavbar.js
@@ -14,7 +14,7 @@ export default function Navbar(props) {
                 className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
                 href="#baseline-dashboard"
               >
-                <img src="/baseline_Horizontal-Logo-allWhite.svg" />
+                <img src="/baselineLogoMark-All-White.png" />
               </a>
             </Link>
             <button
