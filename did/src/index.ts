972b8da624d1779d36e994cbd8ced9f1a038c3ca
Carlos Guimaraes <sekmet@open4g.com>
2021-02-05 23:54:15 -0300

    Delete did directory

--- did/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @public
- */
-const schema = require('../plugin.schema.json')
-export { schema }
-export { DIDConfigurationPlugin } from './agent/did-configuration-plugin'
-export * from './types/IWellKnownDidConfigurationPlugin'
